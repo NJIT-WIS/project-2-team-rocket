1e6f081832ca3c3509d8569b2b5f22cbe3884cf8
KaanIsmet <kaanismet7@gmail.com>
2023-04-20 17:14:15 -0400

    added account page. Modified pages.json so it will test on more pages. Changed blogs title.

---
+++ pages/account.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+
+export default function account() {
+    return(
+        <Layout>
+            <Head>
+                <title>{siteTitle} | Account</title>
+            </Head>
+        </Layout>
+    )
+}

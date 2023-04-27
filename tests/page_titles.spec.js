159a9371765938c39c8946b6ef7a4a435304729c
ZachGG421 <chubbizg@gmail.com>
2023-04-27 15:59:40 -0400

    updated page.titles.spec.js

--- tests/page_titles.spec.js
+++ tests/page_titles.spec.js
@@ -25,12 +25,3 @@ pages.forEach((page) => {
     await checkPageTitle(pageUrl, expectedTitle);
   });
 });
-
-/*
-test("Test case 1: Click on about page", async ({ page }) => {
-  await page.goto('njit-wis.github.io/project-2-team-rocket/');
-  await page.getByText('About ▼').click();
-
-  expect(page.url()).toBe('njit-wis.github.io/project-2-team-rocket/about');
-});
-*/

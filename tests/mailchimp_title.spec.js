d5975182a022290d0fe6d325ffc7ac031d1e7a29
ZachGG421 <chubbizg@gmail.com>
2023-05-10 14:02:30 -0400

    fixing test 1

--- tests/mailchimp_title.spec.js
+++ tests/mailchimp_title.spec.js
@@ -1,12 +1,30 @@
-const {test, expect} = require('@playwright/test')
+const { test, expect } = require('@playwright/test');
 const { chromium, webkit } = require('playwright');
-const path = require('path');
 
-const config = require(path.join(process.cwd(), 'playwright.config.js'));
+// Configure Playwright to use the appropriate browser
+const browserName = process.env.BROWSER || 'chromium';
 
+// Define the test case
 test('Test Case 1: Test mailchimp Navigation', async ({ page }) => {
-    await page.goto('https://njit-wis.github.io/project-2-team-rocket');
-    await page.getByText('About ▼').click();
-    await page.getByRole('link', { name: 'Subscribe' }).click();
-
+  await page.goto('https://njit-wis.github.io/project-2-team-rocket');
+  await page.click('text=About ▼');
+  await page.click('text=Subscribe');
 });
+
+// Run the test
+(async () => {
+  const browser = await { chromium, webkit }[browserName].launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  try {
+    await test.run({
+      page,
+    });
+  } catch (error) {
+    console.error('Test failed:', error);
+  } finally {
+    await context.close();
+    await browser.close();
+  }
+})();

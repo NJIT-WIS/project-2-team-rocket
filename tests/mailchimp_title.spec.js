500077dfbd688e70dca28eb05d2465fb60ed5cde
ZachGG421 <chubbizg@gmail.com>
2023-05-10 15:22:56 -0400

    fixing test 1

--- tests/mailchimp_title.spec.js
+++ tests/mailchimp_title.spec.js
@@ -1,8 +1,5 @@
 const { test, expect } = require('@playwright/test');
-const { chromium, webkit } = require('playwright');
-
-// Configure Playwright to use the appropriate browser
-const browserName = process.env.BROWSER || 'chromium';
+const { chromium } = require('playwright');
 
 // Define the test case
 test('Test Case 1: Test mailchimp Navigation', async ({ page }) => {

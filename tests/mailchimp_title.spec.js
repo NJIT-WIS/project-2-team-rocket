5a6c44eaed8e47f9a698b7b360fcc45578c7cc12
ZachGG421 <chubbizg@gmail.com>
2023-05-10 13:18:08 -0400

    fixing test 1

--- tests/mailchimp_title.spec.js
+++ tests/mailchimp_title.spec.js
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test'
 
-
+/*
 const {test, expect} = require('@playwright/test')
 const { chromium } = require('playwright');
+*/
 
 test('Test Case 1: Test mailchimp navigation', async ({ page }) => {
     await page.goto('https://njit-wis.github.io/project-2-team-rocket');

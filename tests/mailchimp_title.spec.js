6838d259dfe1f56eb487b334bb6bc13c96410fd2
ZachGG421 <chubbizg@gmail.com>
2023-05-10 12:59:42 -0400

    added test case 1

---
+++ tests/mailchimp_title.spec.js
@@ -0,0 +1,9 @@
+const {test, expect} = require('@playwright/test')
+const { chromium } = require('playwright');
+
+test('Test Case 1: Test mailchimp navigation', async ({ page }) => {
+    await page.goto('https://njit-wis.github.io/project-2-team-rocket');
+    await page.getByText('About ▼').click();
+    await page.getByRole('link', { name: 'Subscribe' }).click();
+
+});

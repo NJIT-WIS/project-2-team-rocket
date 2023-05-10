7e8d08453c0ad17b9f9112a8c38035d15cb401cc
ZachGG421 <chubbizg@gmail.com>
2023-05-10 13:31:15 -0400

    fixing test 1

--- tests/mailchimp_title.spec.js
+++ tests/mailchimp_title.spec.js
@@ -4,7 +4,7 @@ const { chromium } = require('playwright');
 const path = require('path');
 const config = require(path.join(process.cwd(), 'playwright.config.js'));
 
-test('Test Case 1: Test mailchimp navigation', async ({ page }) => {
+test('Test Case 1: Test mailchimp Navigation', async ({ page }) => {
     await page.goto('https://njit-wis.github.io/project-2-team-rocket');
     await page.getByText('About ▼').click();
     await page.getByRole('link', { name: 'Subscribe' }).click();

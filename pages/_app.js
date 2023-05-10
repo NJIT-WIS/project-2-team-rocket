c84b357a3c1e23bc2d456a1946cb099b01c1d895
Pufffish <alliehe2015@gmail.com>
2023-05-10 13:16:20 -0400

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,20 @@ function MyApp({ Component, pageProps }) {
 
 
 
+  <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LERWW2ZVSY"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LERWW2ZVSY');
+        `}
+      </Script>
+
   return <Component {...pageProps} />
 }
 

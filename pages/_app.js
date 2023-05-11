153eb6324939d4240ed5efffbbf42a0178c40974
KaanIsmet <kaanismet7@gmail.com>
2023-05-10 20:55:05 -0400

    added layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 function MyApp({ Component, pageProps }) {
-
-
-
-  <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-LERWW2ZVSY"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-LERWW2ZVSY');
-        `}
-      </Script>
-
   return <Component {...pageProps} />
 }
 
